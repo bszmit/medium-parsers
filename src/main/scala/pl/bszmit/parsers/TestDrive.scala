31ce5228d770cd2a12dd7a4d8481041194e5ab81
Bartosz Szmit <bartosz.szmit@gmail.com>
2020-04-19 15:11:31 +0200

    Multiplication

--- src/main/scala/pl/bszmit/parsers/TestDrive.scala
+++ src/main/scala/pl/bszmit/parsers/TestDrive.scala
@@ -8,18 +8,22 @@ object TestDrive extends App {
   val Expr: Parser[Double] = {
     val p1 = for {
       lhs <- double
+      _   <- char('*')
+      rhs <- Expr
+    } yield lhs * rhs
+
+    val p2 = for {
+      lhs <- double
       _   <- char('+')
       rhs <- Expr
     } yield lhs + rhs
 
-    val p2 = double
+    val p3 = double
 
-    p1 | p2
+    p1 | p2 | p3
   }
 
   List(
-    Expr.parse("1").done, // Done(,1.0)
-    Expr.parse("1+2").done, // Done(,3.0)
-    Expr.parse("1+2+3+4+5").done, // Done(,15.0)
+    Expr.parse("2+2*2+2").done, // Done(,10.0) -- expected 8
   ).foreach(println)
 }

a85601aeb139d563b8451bb6466981d0ab5d9b44
Bartosz Szmit <bartosz.szmit@gmail.com>
2020-04-19 15:08:48 +0200

    Sum of many numbers

--- src/main/scala/pl/bszmit/parsers/TestDrive.scala
+++ src/main/scala/pl/bszmit/parsers/TestDrive.scala
@@ -5,14 +5,21 @@ import atto._
 
 object TestDrive extends App {
 
-  val Expr: Parser[Double] = for {
-    lhs <- double
-    _   <- char('+')
-    rhs <- double
-  } yield lhs + rhs
+  val Expr: Parser[Double] = {
+    val p1 = for {
+      lhs <- double
+      _   <- char('+')
+      rhs <- Expr
+    } yield lhs + rhs
+
+    val p2 = double
+
+    p1 | p2
+  }
 
   List(
+    Expr.parse("1").done, // Done(,1.0)
     Expr.parse("1+2").done, // Done(,3.0)
-    Expr.parse("1+2+3").done, // Done(+3,3.0) -- expected 6.0
+    Expr.parse("1+2+3+4+5").done, // Done(,15.0)
   ).foreach(println)
 }

40e0e8310ccc06d6009b44bb645fcd3abe53f0ef
Bartosz Szmit <bartosz.szmit@gmail.com>
2020-04-19 15:04:33 +0200

    Sum of two numbers

--- src/main/scala/pl/bszmit/parsers/TestDrive.scala
+++ src/main/scala/pl/bszmit/parsers/TestDrive.scala
@@ -1,5 +1,18 @@
 package pl.bszmit.parsers
 
+import atto.Atto._
+import atto._
+
 object TestDrive extends App {
-  println("hello quarantine")
+
+  val Expr: Parser[Double] = for {
+    lhs <- double
+    _   <- char('+')
+    rhs <- double
+  } yield lhs + rhs
+
+  List(
+    Expr.parse("1+2").done, // Done(,3.0)
+    Expr.parse("1+2+3").done, // Done(+3,3.0) -- expected 6.0
+  ).foreach(println)
 }

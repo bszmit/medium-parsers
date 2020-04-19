5881898025df1113f434aeede4acbd683499afa4
Bartosz Szmit <bartosz.szmit@gmail.com>
2020-04-19 15:16:07 +0200

    Operators precedence

--- src/main/scala/pl/bszmit/parsers/TestDrive.scala
+++ src/main/scala/pl/bszmit/parsers/TestDrive.scala
@@ -5,25 +5,31 @@ import atto._
 
 object TestDrive extends App {
 
-  val Expr: Parser[Double] = {
+  lazy val Expr: Parser[Double] = {
     val p1 = for {
-      lhs <- double
-      _   <- char('*')
-      rhs <- Expr
-    } yield lhs * rhs
-
-    val p2 = for {
-      lhs <- double
+      lhs <- Term
       _   <- char('+')
       rhs <- Expr
     } yield lhs + rhs
+    val p2 = Term
 
-    val p3 = double
+    p1 | p2
+  }
 
-    p1 | p2 | p3
+  lazy val Term: Parser[Double] = {
+    val p1 = for {
+      lhs <- double
+      _   <- char('*')
+      rhs <- Term
+    } yield lhs * rhs
+    val p2 = double
+    p1 | p2
   }
 
   List(
-    Expr.parse("2+2*2+2").done, // Done(,10.0) -- expected 8
+    Expr.parse("2+2*2").done, // Done(,6.0)
+    Expr.parse("2*2+2").done, // Done(,6.0)
+    Expr.parse("2*2+3*2").done, // Done(,10.0)
+    Expr.parse("2+2*2+2").done, // Done(,8.0)
   ).foreach(println)
 }
